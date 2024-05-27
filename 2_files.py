32b9894093377f449c5c3f5ab1c45451c64e9ee1
Sorakaze Aki <soraaki000@gmail.com>
2024-05-27 16:36:16 +0500

    Задание 2

--- 2_files.py
+++ 2_files.py
@@ -5,18 +5,25 @@
 
 
 1. Скачайте файл по ссылке https://www.dropbox.com/s/sipsmqpw1gwzd37/referat.txt?dl=0
-2. Прочитайте содержимое файла в перменную, подсчитайте длинну получившейся строки
+2. Прочитайте содержимое файла в переменную, подсчитайте длину получившейся строки
 3. Подсчитайте количество слов в тексте
 4. Замените точки в тексте на восклицательные знаки
 5. Сохраните результат в файл referat2.txt
 """
 
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    words, content = 0, []
+    with open("referat.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            words += len(line.split())
+            content.append(line.replace('.', '!'))
+    full_text = ''.join(content)
+    print(f"Длина {len(full_text)} символов")
+    print(f"В файле {words} слова")
+    with open("referat2.txt", "w", encoding="utf-8") as f:
+        f.write(full_text)
+
 
 if __name__ == "__main__":
     main()
